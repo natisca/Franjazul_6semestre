5d2d14312d229617ec2fde9216768993cc0c199f
Juan Restrepo C <juan_restrepo23202@elpoli.edu.co>
2025-08-18 18:35:07 -0500

    Se organizan las carpetas, se puede acceder a la ruta "/login", sin funcionalidad.

--- Franjazul/src/app/app.routes.ts
+++ Franjazul/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    
+    {
+        path: 'login',
+        loadComponent: ()=> import('./componentes/autenticacion/login/login')
+    }
+
+];
--- Franjazul/src/app/app.ts
+++ Franjazul/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, provideRouter } from '@angular/router';
+import { Navbar } from "./componentes/estaticos/navbar/navbar";
+import { Footer } from "./componentes/estaticos/footer/footer";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

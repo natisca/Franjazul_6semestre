dd117999bd9f9755d02c0d2f465e8b85592b2b0d
Juan Restrepo C <juan_restrepo23202@elpoli.edu.co>
2025-09-22 11:44:27 -0500

    Se agrega un footer basico

--- Franjazul/src/app/app.ts
+++ Franjazul/src/app/app.ts
@@ -4,12 +4,13 @@ import { Login } from "./componentes/Autenticacion/login/login";
 import { Sidebar } from "./componentes/Reutilizables/sidebar/sidebar";
 import { Header } from './componentes/Reutilizables/header/header';
 import { home } from './componentes/Negocio/home/home';
+import { Footer } from "./componentes/Reutilizables/footer/footer";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Sidebar, Header, home],
+  imports: [RouterOutlet, Login, Sidebar, Header, home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Franjazul/src/app/componentes/Reutilizables/footer/footer.ts
+++ Franjazul/src/app/componentes/Reutilizables/footer/footer.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrls: ['./footer.css']
 })
 export class Footer {
 
